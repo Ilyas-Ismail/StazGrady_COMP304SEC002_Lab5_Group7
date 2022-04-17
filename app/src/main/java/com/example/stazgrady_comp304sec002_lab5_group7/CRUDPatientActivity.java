e5c7e3ba37cb0dc44c05c8d8cf14b8a0a279dc94
Ilyas-Ismail <ilyasismaiil@hotmail.com>
2022-04-16 22:11:20 -0400

    All CRUD operations added
    User can create, read, update, and delete patient information from the firebase database.

--- app/src/main/java/com/example/stazgrady_comp304sec002_lab5_group7/CRUDPatientActivity.java
+++ app/src/main/java/com/example/stazgrady_comp304sec002_lab5_group7/CRUDPatientActivity.java
@@ -1,21 +1,37 @@
 package com.example.stazgrady_comp304sec002_lab5_group7;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.HashMap;
+import java.util.Locale;
 
 public class CRUDPatientActivity extends AppCompatActivity {
+
+    private DatabaseReference dbReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crudpatient);
+
+        FirebaseDatabase db = FirebaseDatabase.getInstance();
+        dbReference = db.getReference(Patient.class.getSimpleName());
 
         EditText nameEdit = findViewById(R.id.nameEdit);
         EditText ageEdit = findViewById(R.id.ageEdit);
@@ -25,38 +41,118 @@ public class CRUDPatientActivity extends AppCompatActivity {
         Button addBtn = findViewById(R.id.addBtn);
         Button updateBtn = findViewById(R.id.updateBtn);
         Button deleteBtn = findViewById(R.id.deleteBtn);
+        Button readBtn = findViewById(R.id.readBtn);
 
         PatientDAO dao = new PatientDAO();
 
+        //if all fields are filled, add the patient into the database once button is pressed
         addBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Patient patient = new Patient(nameEdit.getText().toString(),
-                        Integer.parseInt(ageEdit.getText().toString()),
-                        diseaseEdit.getText().toString(),
-                        Double.parseDouble(billEdit.getText().toString()));
 
-                dao.insert(patient);
+                if(!TextUtils.isEmpty(nameEdit.getText().toString()) &&
+                        !TextUtils.isEmpty(ageEdit.getText().toString()) &&
+                        !TextUtils.isEmpty(diseaseEdit.getText().toString()) &&
+                        !TextUtils.isEmpty(billEdit.getText().toString())) {
+
+                    Patient patient = new Patient(nameEdit.getText().toString(),
+                            Integer.parseInt(ageEdit.getText().toString()),
+                            diseaseEdit.getText().toString(),
+                            Double.parseDouble(billEdit.getText().toString()));
+
+                    dao.insert(patient).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(CRUDPatientActivity.this, getResources().getString(R.string.successAdd), Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(CRUDPatientActivity.this, getResources().getString(R.string.failure), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                } else {
+                    Toast.makeText(CRUDPatientActivity.this, getResources().getString(R.string.emptyFields), Toast.LENGTH_SHORT).show();
+                }
+
             }
         });
 
+        //if all fields are filled, update the patient from the database
         updateBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                HashMap<String, Object> hashMap = new HashMap<>();
-                hashMap.put("name", nameEdit.getText().toString());
-                hashMap.put("age", Integer.parseInt(ageEdit.getText().toString()));
-                hashMap.put("disease", diseaseEdit.getText().toString());
-                hashMap.put("bill", Double.parseDouble(billEdit.getText().toString()));
 
-                dao.update(nameEdit.getText().toString(), hashMap);
+                if(!TextUtils.isEmpty(nameEdit.getText().toString()) &&
+                        !TextUtils.isEmpty(ageEdit.getText().toString()) &&
+                        !TextUtils.isEmpty(diseaseEdit.getText().toString()) &&
+                        !TextUtils.isEmpty(billEdit.getText().toString())) {
+
+                    HashMap<String, Object> hashMap = new HashMap<>();
+                    hashMap.put("name", nameEdit.getText().toString());
+                    hashMap.put("age", Integer.parseInt(ageEdit.getText().toString()));
+                    hashMap.put("disease", diseaseEdit.getText().toString());
+                    hashMap.put("bill", Double.parseDouble(billEdit.getText().toString()));
+
+                    dao.update(nameEdit.getText().toString(), hashMap).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        @Override
+                        public void onSuccess(Void unused) {
+                            Toast.makeText(CRUDPatientActivity.this, getResources().getString(R.string.successUpdate), Toast.LENGTH_SHORT).show();
+                        }
+                    }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(CRUDPatientActivity.this, getResources().getString(R.string.failure), Toast.LENGTH_SHORT).show();
+                        }
+                    });
+
+                } else {
+                    Toast.makeText(CRUDPatientActivity.this, getResources().getString(R.string.emptyFields), Toast.LENGTH_SHORT).show();
+                }
             }
         });
 
+        //deletes the patient from the database based on name
         deleteBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                dao.delete(nameEdit.getText().toString());
+                dao.delete(nameEdit.getText().toString()).addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(CRUDPatientActivity.this, getResources().getString(R.string.successDelete), Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(CRUDPatientActivity.this, getResources().getString(R.string.failure), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
+
+        //checks if the database has an entry with the inputted name, if so, display data in appropriate fields
+        readBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dbReference.child(nameEdit.getText().toString().toUpperCase(Locale.ROOT)).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        if(snapshot.exists()) {
+                            Patient patient = snapshot.getValue(Patient.class);
+                            ageEdit.setText(String.valueOf(patient.getAge()));
+                            diseaseEdit.setText(patient.getDisease());
+                            billEdit.setText(String.valueOf(patient.getBill()));
+                        } else {
+                            Toast.makeText(CRUDPatientActivity.this, getResources().getString(R.string.none), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });
             }
         });
     }
--- app/src/main/java/com/example/stazgrady_comp304sec002_lab5_group7/PatientDAO.java
+++ app/src/main/java/com/example/stazgrady_comp304sec002_lab5_group7/PatientDAO.java
@@ -1,10 +1,16 @@
 package com.example.stazgrady_comp304sec002_lab5_group7;
 
+import androidx.annotation.NonNull;
+
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
+import java.util.Locale;
 import java.util.Map;
 
 public class PatientDAO {
@@ -18,16 +24,18 @@ public class PatientDAO {
 
     //insert a patient into the database with their name as the key
     public Task<Void> insert(Patient patient) {
-        return dbReference.child(patient.getName()).setValue(patient);
+        return dbReference.child(patient.getName().toUpperCase(Locale.ROOT)).setValue(patient);
     }
 
     //update the patient based on the name
     public Task<Void> update(String key, HashMap<String, Object> hashMap) {
-        return dbReference.child(key).updateChildren(hashMap);
+        return dbReference.child(key.toUpperCase(Locale.ROOT)).updateChildren(hashMap);
     }
 
     //delete the patient based on the name
     public Task<Void> delete(String key) {
-        return dbReference.child(key).removeValue();
+        return dbReference.child(key.toUpperCase(Locale.ROOT)).removeValue();
     }
+
+
 }
